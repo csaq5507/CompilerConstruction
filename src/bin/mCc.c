beed45d832de5c73ff621a4a8c74578c50de9fac
Ivan Waldboth <Ivan.Waldboth@student.uibk.ac.at>
2018-05-02 13:19:36 +0200

    stderr

--- src/bin/mCc.c
+++ src/bin/mCc.c
@@ -18,6 +18,11 @@ void print_usage(const char *prg)
 
 int main(int argc, char *argv[])
 {
+    FILE *l;
+    l=fopen("std.err","a");
+
+    fprintf(l,"\n---------------------------------------------------------\n"
+            "%s\n",argv[1]);
 	if (argc < 2) {
 		print_usage(argv[0]);
 		return EXIT_FAILURE;
@@ -43,75 +48,79 @@ int main(int argc, char *argv[])
 		fclose(in);
 		if (result.status == MCC_PARSER_STATUS_ERROR) {
 			if (DEBUG) {
-				printf("Parser_error:\n");
+				fprintf(l, "Parser_error:\n");
 				for (int i = 0; i < result.errors->counter;
 				     i++) {
-					printf("Error at line %d: ",
+					fprintf(l, "Error at line %d: ",
 					       result.errors->errors[i]
 						       .error_line);
-					printf("%s\n", result.errors->errors[i]
+					fprintf(l, "%s\n", result.errors->errors[i]
 							       .error_msg);
 				}
 				mCc_ast_delete_function_def_array(
 					result.func_def);
 				mCc_delete_result(&result);
+                fclose(l);
 				return EXIT_SUCCESS;
 			}
 		}
 		func = result.func_def;
 
 		FILE *out1;
-		FILE *out2;
-		out1 = fopen("output1.txt", "w");
-		out2 = fopen("output2.txt", "w");
+        char str[80];
+      //  strcpy(str,argv[1]);
+      //  strcat(str,".graph");
+      //  out1 = fopen(str, "w");
 
-		mCc_ast_print_dot_function_def(out1, func);
 
 		result = *(mCc_ast_symbol_table(&result));
 
-		mCc_ast_print_dot_function_def(out2, result.func_def);
+	//	mCc_ast_print_dot_function_def(out1, result.func_def);
 
-		fclose(out1);
-		fclose(out2);
+	//	fclose(out1);
 
 		if (result.status == MCC_PARSER_STATUS_ERROR) {
 			if (DEBUG) {
-				printf("Parser_error:\n");
+				fprintf(l, "Semantic_error:\n");
 				for (int i = 0; i < result.errors->counter;
 				     i++) {
-					printf("Error at line %d: ",
+					fprintf(l, "Error at line %d: ",
 					       result.errors->errors[i]
 						       .error_line);
-					printf("%s\n", result.errors->errors[i]
+					fprintf(l, "%s\n", result.errors->errors[i]
 							       .error_msg);
 				}
 				mCc_ast_delete_function_def_array(
 					result.func_def);
 				mCc_delete_result(&result);
+                fclose(l);
 				return EXIT_SUCCESS;
 			}
 		}
 
 		result = *(mCc_ast_semantic_check(&result));
 
-		if (result.status == MCC_PARSER_STATUS_ERROR) {
+            if (result.status == MCC_PARSER_STATUS_ERROR) {
 			if (DEBUG) {
-				printf("Parser_error:\n");
+				fprintf(l, "Semantic_error:\n");
 				for (int i = 0; i < result.errors->counter;
 				     i++) {
-					printf("Error at line %d: ",
+					fprintf(l, "Error at line %d: ",
 					       result.errors->errors[i]
 						       .error_line);
-					printf("%s\n", result.errors->errors[i]
+					fprintf(l, "%s\n", result.errors->errors[i]
 							       .error_msg);
 				}
 				mCc_ast_delete_function_def_array(
 					result.func_def);
 				mCc_delete_result(&result);
+                fclose(l);
 				return EXIT_SUCCESS;
 			}
 		}
 
+        fprintf(l,"No Errors\n");
+        fclose(l);
 		mCc_ast_delete_function_def_array(result.func_def);
 		mCc_delete_result(&result);
 	}

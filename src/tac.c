4096c0c8b4d56b1248920794e1d375b1cd4f61b3
daniel.kopp@student.uibk.ac.at <y8GHNPXcR3BDKLQa>
2018-07-23 19:19:28 +0200

    tac

--- src/tac.c
+++ src/tac.c
@@ -677,35 +677,47 @@ static void handle_equal_identifier_name(struct mCc_ast_expression *expression)
     new_param_lhs->type = MCC_TAC_ELEMENT_TYPE_PARAMETER_SETUP;
     new_param_lhs->identifier1 = new_string("var_tmp_%d", v_counter++);
     new_param_lhs->param_size = 1;
-    tac_list *new_copy_lhs = tac_new_list();
-    new_copy_lhs->type = MCC_TAC_ELEMENT_TYPE_COPY_IDENTIFIER;
-    new_copy_lhs->identifier1 = copy_string(new_param_lhs->identifier1);
-    new_copy_lhs->copy_identifier = copy_string(temp_lhs_end->identifier1);
+    tac_list *new_copy_lhs1 = tac_new_list();
+    tac_list *new_copy_lhs2 = tac_new_list();
+    new_copy_lhs2->type = MCC_TAC_ELEMENT_TYPE_COPY_IDENTIFIER;
+    new_copy_lhs2->identifier1 = new_string("reg_%d", v_counter++);
+    new_copy_lhs2->copy_identifier = copy_string(new_param_lhs->identifier1);
+    new_copy_lhs1->type = MCC_TAC_ELEMENT_TYPE_COPY_IDENTIFIER;
+    new_copy_lhs1->identifier1 = copy_string(new_param_lhs->identifier1);
+    new_copy_lhs1->copy_identifier = copy_string(temp_lhs_end->identifier1);
 
     tac_list *new_param_rhs = tac_new_list();
     new_param_rhs->type = MCC_TAC_ELEMENT_TYPE_PARAMETER_SETUP;
     new_param_rhs->identifier1 = new_string("var_tmp_%d", v_counter++);
     new_param_rhs->param_size = 1;
-    tac_list *new_copy_rhs = tac_new_list();
-    new_copy_rhs->type = MCC_TAC_ELEMENT_TYPE_COPY_IDENTIFIER;
-    new_copy_rhs->identifier1 = copy_string(new_param_rhs->identifier1);
-    new_copy_rhs->copy_identifier = copy_string(temp_rhs_end->identifier1);
+    tac_list *new_copy_rhs1 = tac_new_list();
+    tac_list *new_copy_rhs2 = tac_new_list();
+    new_copy_rhs2->type = MCC_TAC_ELEMENT_TYPE_COPY_IDENTIFIER;
+    new_copy_rhs2->identifier1 = new_string("reg_%d", v_counter++);
+    new_copy_rhs2->copy_identifier = copy_string(new_param_rhs->identifier1);
+    new_copy_rhs1->type = MCC_TAC_ELEMENT_TYPE_COPY_IDENTIFIER;
+    new_copy_rhs1->identifier1 = copy_string(new_param_rhs->identifier1);
+    new_copy_rhs1->copy_identifier = copy_string(temp_rhs_end->identifier1);
 
-    new_param_lhs->next = new_copy_lhs;
-    new_copy_lhs->prev = new_copy_lhs;
-    new_copy_lhs->next = temp_lhs_end->next;
+    new_param_lhs->next = new_copy_lhs1;
+    new_copy_lhs1->prev = new_param_lhs;
+    new_copy_lhs1->next = new_copy_lhs2;
+    new_copy_lhs2->prev = new_copy_lhs1;
+    new_copy_lhs2->next = temp_lhs_end->next;
     temp_lhs_end->next = new_param_lhs;
     new_param_lhs->prev = temp_lhs_end;
-    expression->lhs->tac_end = new_param_lhs;
-    push_tac_end_single_expression(expression->lhs, new_copy_lhs);
+    expression->lhs->tac_end = new_copy_lhs2;
+    push_tac_end_single_expression(expression->lhs, new_copy_lhs2);
 
-    new_param_rhs->next = new_copy_rhs;
-    new_copy_rhs->prev = new_copy_rhs;
-    new_copy_rhs->next = temp_rhs_end->next;
+    new_param_rhs->next = new_copy_rhs1;
+    new_copy_rhs1->prev = new_param_rhs;
+    new_copy_rhs1->next = new_copy_rhs2;
+    new_copy_rhs2->prev = new_copy_rhs1;
+    new_copy_rhs2->next = temp_rhs_end->next;
     temp_rhs_end->next = new_param_rhs;
     new_param_rhs->prev = temp_rhs_end;
-    expression->rhs->tac_end = new_param_rhs;
-    push_tac_end_expression(expression->rhs, new_copy_rhs);
+    expression->rhs->tac_end = new_copy_rhs2;
+    push_tac_end_expression(expression->rhs, new_copy_rhs2);
 }
 
 static void tac_expression(struct mCc_ast_expression *expression, void *data)
